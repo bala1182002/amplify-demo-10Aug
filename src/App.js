3e9c2880b5d978805b761d78e6c821013f667fda
Balasubramanian Ganapathy <63049128+bala1182002@users.noreply.github.com>
2024-08-10 19:42:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Amplify demo 
+          Amplify demo user-1 branch
         </a>
       </header>
     </div>
